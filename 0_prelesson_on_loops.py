cd53d484370cccb8a609817757cbe9e3f2da3705
Anthony Guzman <156100317+AnthonyGooseMan@users.noreply.github.com>
2025-12-09 20:21:49 +0000

    Loops

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,26 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+# for loops = execite a block of colde a fixed number if tune
+# you can iterate over a range, string, sequence, etc.
+
+# for x in reversed(range(1, 11)):
+#     print(x)
+
+# print("HAPPY NEW YEAR!")
+
+# credit_card = "5211-0500-1665-5621"
+# for x in credit_card:
+#     print(x)
+
+# # for x in range(1,21):
+# #     if x == 13:
+# #         continue
+# # else:
+# #     print(x)
+
+# for x in range(1,21):
+#     if x == 13:
+#         break
+# else:
+#     print(x)
+
